4e61c31e9acbfb18ee2cd8608080c1fadc87bc0c
milanpjevic <milanpjevic@gmail.com>
2020-12-07 22:48:08 +0100

    Create Sample Users in MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+	users: [
+		{
+			name: "Milan",
+			email: "admin@amazona.com",
+			password: bcrypt.hashSync("1234", 8),
+			isAdmin: true,
+		},
+		{
+			name: "John",
+			email: "user@amazona.com",
+			password: bcrypt.hashSync("1234", 8),
+			isAdmin: false,
+		},
+	],
 	products: [
 		{
 			_id: 1,
